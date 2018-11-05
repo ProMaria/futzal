cd43d6ae56586916a0a7d7328cf32fafc27f67e7
masha <masha@debian.debianhome>
2018-11-05 17:40:30 +0300

    add home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
       @items =Item.last(3)
+      @results = Schedule.where("result !=''").last(10)
+      @schedule = Schedule.where("result =''").last(10)
   end
 end
