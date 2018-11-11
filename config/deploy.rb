b05c6105f9a427829c459a1e9f25837df544b172
masha <masha@debian.debianhome>
2018-11-12 01:12:42 +0300

    image

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,8 @@
+require 'mina/bundler' 
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,7 +11,7 @@ require 'mina/git'
 #   branch       - Branch name to deploy. (needed by mina/git)
 
 set :application_name, 'koaf-futzal'
-set :domain, 'koaf-futzal.ru'
+set :domain, '185.219.43.24'
 set :deploy_to, '/home/masha/www/'
 set :repository, 'https://github.com/ProMaria/futzal.git'
 set :branch, 'master'
@@ -23,8 +24,8 @@ set :branch, 'master'
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push('public/assets', 'public/system')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'log')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/assets', 'public/system', 'log')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
