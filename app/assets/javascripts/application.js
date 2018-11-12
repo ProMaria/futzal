ccfba48b282645804722cc6872b3f616e59c3c41
masha <masha@debian.debianhome>
2018-11-12 23:23:47 +0300

    delete scroll from app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,18 +20,4 @@
 //= require activestorage
 //= require turbolinks
 
-$(document).ready(function(){   
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('#scroller').fadeIn();
-        } else {
-            $('#scroller').fadeOut();
-        }
-    });
-    $('#scroller').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
-});
+
