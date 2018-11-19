e2d51a2f0f09b47cdf9d3ea271374c52f4a593b6
Proshakova Mariya <proshakova@miac.kaluga.ru>
2018-11-19 10:23:39 +0300

    change team image big format

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,7 +11,7 @@ class Team < ApplicationRecord
         Schedule.where('home_team_id=? or guest_team_id=?', self.id, self.id)
     end
     
-    has_attached_file :image, {:styles => { :small => "150x150>",:big => "300x300>", thumb:  "30%X30%"},
+    has_attached_file :image, {:styles => { :small => "150x150>",:big => "650x650>"},
             
     url: "/system/:hash.:extension",
     hash_secret: "dc6360523aa19bd9913d64b7ebc83009c09ab5e48814f713c3027e92bf457b0d70f5f3f8d8ec1adc6450664198dc0090ece075c54eb852def1147e972f9ed64d",
