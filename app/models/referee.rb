8b60e7b6fcad3ff23a208814a63be7943b3782f9
Proshakova Mariya <proshakova@miac.kaluga.ru>
2018-12-11 09:06:46 +0300

    add referree foto

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -1,5 +1,5 @@
 class Referee < ApplicationRecord
-    has_attached_file :image, {:styles => { :small => "150x150>" },
+    has_attached_file :image, {:styles => { :small => "150x150>",:big => "650x650>"},
             
     url: "/system/:hash.:extension",
     hash_secret: "dc6360523aa19bd9913d64b7ebc83009c09ab5e48814f713c3027e92bf457b0d70f5f3f8d8ec1adc6450664198dc0090ece075c54eb852def1147e972f9ed64d",
@@ -11,5 +11,5 @@ class Referee < ApplicationRecord
     
     validates_attachment :image,
                      content_type: { content_type: /\Aimage\/.*\z/ },
-                     size: { less_than: 1.megabyte }
+                     size: { less_than: 5.megabyte }
 end
